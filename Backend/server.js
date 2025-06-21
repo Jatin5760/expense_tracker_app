cde3488f418c474b639e796fb9a95179171398e5
Jatin5760 <jatinsharma14202003@gmail.com>
2025-06-21 23:19:32 +0530

    Commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.get('/', (req,res)=>{
     res.send('Hello it is working');
 })
-app.listen(5001,()=>{
-    console.log("Server is running on port 5001");
+
+const PORT = process.env.PORT;
+//console.log("my port:", process.env.PORT);
+
+app.listen(PORT,()=>{
+    console.log("Server is running on port", PORT);
 })
